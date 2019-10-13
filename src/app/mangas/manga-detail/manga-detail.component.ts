1bdd25153362166cb979040a17a5f272bf348f81
Alberto Garcia <albertoinformatica97@gmail.com>
2019-10-13 17:46:46 +0200

    Ya se muestran los detalles de los mangas

--- src/app/mangas/manga-detail/manga-detail.component.ts
+++ src/app/mangas/manga-detail/manga-detail.component.ts
@@ -1,15 +1,35 @@
 import { Component, OnInit } from '@angular/core';
+import { AnimesService } from 'src/app/animes.service';
+import { ActivatedRoute } from '@angular/router';
+import { MangaDetail } from 'src/app/interfaces/manga-detail';
+
 
 @Component({
   selector: 'app-manga-detail',
   templateUrl: './manga-detail.component.html',
   styleUrls: ['./manga-detail.component.scss']
 })
+
+
 export class MangaDetailComponent implements OnInit {
 
-  constructor() { }
+  constructor(
+    private animeService:AnimesService,
+    private route:ActivatedRoute
+  ) { }
 
   ngOnInit() {
+    this.getMangaById()
+  }
+
+  manga:MangaDetail
+
+  getMangaById() {
+    const id = +this.route.snapshot.paramMap.get('id')
+    this.animeService.getMangaById(id)
+      .subscribe(manga => {
+        this.manga = manga
+      })
   }
 
 }
